cbb9e6d5fb6cb618da90464faf6ec8af9bfeb25d
Michael Padiernos <mike.padiernos@gmail.com>
2020-04-24 12:17:10 -0400

    [REFACTOR] API / Session
    - configured code to have sessionConfig in separate file as user-session.js

--- api/server.js
+++ api/server.js
@@ -5,32 +5,13 @@ const session = require('express-session');
 const knexSessionStore = require('connect-session-knex')(session);
 
 const middle = require('./middleware/middleware.js');
+const config = require('./session/user-session.js');
 
 const logger = middle.logger;
+const sessionConfig = config.sessionConfig;
 
 const server = express();
 
-const sessionConfig = {
-	name: 'fudge-stripes-minis',
-	secret: 'supercalifragilisticexpialidocious',
-	cookie: {
-		maxAge: 3600 * 1000,
-		secure: false, // should be true in production
-		httpOnly: true
-	},
-	resave: false,
-	saveUninitialized: false,
-
-	store: new knexSessionStore(
-		{
-			knex: require("../data/db-config.js"),
-			tablename: "sessions",
-			sidfieldname: "sid",
-			createtable: true,
-			clearInterval: 3600 * 1000
-		}
-	)
-}
 server.use(helmet(), logger, express.json(), cors());
 
 const users = require('./routers/users-router.js');
